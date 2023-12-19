9e4f0b1d012d5d454fd2193dcf88fd1b96087756
salmaelabdellaoui <salma_el_abdellaoui@hotmail.com>
2023-12-19 13:12:10 +0100

    x

---
+++ 07_Phyton/07_02_Variables.py
@@ -0,0 +1,14 @@
+# Stap 1: Maak twee variabelen x en y en ken numerieke waarden toe
+x = 5
+y = 10
+
+# Stap 2: Druk de waarden van x en y af
+print("Waarde van x:", x)
+print("Waarde van y:", y)
+
+# Stap 3: Maak een derde variabele z en ken de som van x en y toe
+z = x + y
+
+# Stap 4: Druk de waarde van z af
+print("Waarde van z (som van x en y):", z)
+
