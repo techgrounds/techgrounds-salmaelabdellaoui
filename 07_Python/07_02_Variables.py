641ecc31d1c392fe7b2778e5d1cd03b9c86738d1
salmaelabdellaoui <salma_el_abdellaoui@hotmail.com>
2023-12-19 13:35:29 +0100

    update

--- 07_Python/07_02_Variables.py
+++ 07_Python/07_02_Variables.py
@@ -1,3 +1,4 @@
+# Exercise 1
 # Stap 1: Maak twee variabelen x en y en ken numerieke waarden toe
 x = 5
 y = 10
@@ -11,4 +12,32 @@ z = x + y
 
 # Stap 4: Druk de waarde van z af
 print("Waarde van z (som van x en y):", z)
+
+# Excercise 2
+# Create a variable called name. The value of name should be your name.
+
+name = "salma"  
+
+# Print the text “Hello, YOURNAME!”.
+
+print("Hello, " + name + "!")
+
+# Excercise 3 
+# Create a variable and assign a value to it
+my_variable = "Salma"
+
+# Print the text “Value 1: VALUE1”
+print("Value 1:", my_variable)
+
+# Change the value of the variable
+my_variable = "25"
+
+# Print the text “Value 2: VALUE2”
+print("Value 2:", my_variable)
+
+# Change the value of the variable again
+my_variable = "Den Haag"
+
+# Print the text “Value 3: VALUE3”
+print("Value 3:", my_variable)
 
