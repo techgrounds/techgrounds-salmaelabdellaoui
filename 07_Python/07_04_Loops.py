474806e811a5a46ac164d6c79684eb414d0194a0
salmaelabdellaoui <salma_el_abdellaoui@hotmail.com>
2023-12-19 15:22:46 +0100

    commit

---
+++ 07_Python/07_04_Loops.py
@@ -0,0 +1,33 @@
+# Excercise 1
+# Create a variable x and give it the value 0
+x = 0
+
+# Use a while loop to print the value of x and increment it until x is greater than 10
+while x <= 10:
+    print("Current value of x:", x)
+    x += 1  # Increment x by 1 in each iteration
+
+
+# Exercise 2   
+# Add a variable x with value 5 at the top of your script
+x = 5
+
+# Using a for loop with range(10)
+for i in range(10):
+    # Print the value of i in the for loop
+    print("Value of i:", i)
+
+# Using a for loop with range(50) to print the value of x multiplied by i
+for i in range(50):
+    result = x * i
+    print(f"Value of x * i for iteration {i}: {result}")
+
+
+# Exercise 3 
+# Copy the array into your script
+arr = ["Shikha", "Casper", "Bart", "Ruben", "Ulviye"]
+
+# Use a for loop to loop over the array and print every name individually
+for name in arr:
+    print(name)
+
